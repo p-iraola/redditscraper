fe781066e0e6d2404c72251ef7f18ed20b125727
p-iraola <102985671+p-iraola@users.noreply.github.com>
2022-10-12 09:05:04 -0400

    Add files via upload

---
+++ reddit_scrape.py
@@ -0,0 +1,23 @@
+import praw
+from collections import Counter
+import re
+
+reddit_read_only = praw.Reddit(client_id ='',
+							   client_secret ='', 
+							   user_agent ='')
+
+
+url = ''
+
+submission = reddit_read_only.submission(url=url)
+
+post_comments = ''
+submission.comments.replace_more(limit=None)
+for comment in submission.comments.list():
+	post_comments += comment.body
+
+
+post_comments = post_comments.lower().split()
+
+
+print(Counter(post_comments))
